f20cfccc489a1c791af5224557dd5f9c27646e79
DKSHS <88355080+ShadowsS01@users.noreply.github.com>
2022-11-14 12:21:18 -0300

    Update Cursos-Django: container initiation

--- Cursos-Django/.docker/start.sh
+++ Cursos-Django/.docker/start.sh
@@ -1,4 +1,13 @@
-#!/bin/bash
+#!/bin/zsh
 
+# Loading colors
+autoload -U colors
+colors
+
+# # To do the migrations
 # python manage.py migrate
+
+echo "$bold_color$fg[green]Aplicação iniciada${reset_color}"
+echo "$bold_color$fg[yellow]Acesse: http://localhost:8080/ \n${reset_color}"
+
 python manage.py runserver 0.0.0.0:8080
